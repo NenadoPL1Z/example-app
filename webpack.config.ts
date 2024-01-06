dab8ed3f400fcc2b7305842a2db4dd137375ebe1
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-01-06 19:59:53 +0500

    update: wepback mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,8 +2,9 @@ import {BuildConfiguration} from "./config/build/types/build";
 import buildConfig from "./config/build";
 import {BuildMode, BuildPaths} from "./config/build/types/config";
 import getPathDirname from "./config/helpers/getPathFromDirname";
+import * as process from "process";
 
-const mode: BuildMode = "production"
+const mode: BuildMode = (process.env.NODE_ENV as BuildMode) || "none";
 
 const paths: BuildPaths = {
     entry: getPathDirname("src", "index.ts"),
